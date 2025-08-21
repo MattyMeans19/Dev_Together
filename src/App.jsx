35b60ba713e4991b652218210d9295c750cddbf8
Matt <mattymeans@gmail.com>
2025-08-21 01:02:09 -0600

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react';
+import Logo from "/DevTogether_Logo.png";
+
+
+function App() {
+
+  return (
+    <>
+      <div className='Logo'>
+        <img src={Logo}></img>
+        <h1>Alone, we're good. Together, we're great.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
