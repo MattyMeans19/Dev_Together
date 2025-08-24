36d94cd7dc3ca836545d386c9236abbfffd46842
Matt <mattymeans@gmail.com>
2025-08-24 03:42:11 -0600

    Start to Login page

--- src/Login.jsx
+++ src/Login.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
+import Logo from "/DevTogether_Logo.png";
+import Footer from "./components/Footer.jsx"
 
 function Login(){
+    const [userData, setUserData] = useState(
+        {
+            userName: '',
+            password: ''
+        }
+    )
+
+        function inputUserData(event){
+        const {name, value} = event.target;
+        setUserData(prevUserData => ({
+            ...prevUserData,
+            [name]: value
+        }));
+    }
+
     return(
-        <div>
-            Login
+        <div className="flex flex-col max-w-screen min-h-screen">
+            <div className="w-full place-items-center text-7xl pb-5 bg-(--system-background) bg-linear-(--background-gradient)">
+                <img src={Logo} className="size-[10rem]"></img>
+                <h1>Login</h1>
+            </div>
+            <div className="MediaBox h-[40vh] inline-flex flex-col grow">
+                <input type="text" id="userName" maxLength="12" placeholder="*User Name" 
+                name="userName" value={userData.userName} onChange={inputUserData} required className="inputBox">
+                </input>
+                <input type="password" id="password" minLength="8" title="Only letters and numbers are allowed" placeholder="*Password" 
+                name="password" value={userData.password} onChange={inputUserData} required className="inputBox">
+                </input>
+            </div>
+            <Footer />
         </div>
     )
 }
